d571ad3280dd85eed1b392b1d4fec6460642feae
Ruslan Voronov <voronov.rv.99@yandex.ru>
2023-05-28 16:55:04 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,14 @@ const mongoose = require('mongoose');
 const bodyParse = require('body-parser');
 const { errors } = require('celebrate');
 const routes = require('./routes');
-const cors = require('cors');``
+// const cors = require('cors'); ``
 const errorHandler = require('./middlewares/errorHandler');
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000'
+];
 
 const { PORT = 3000 } = process.env;
 
@@ -13,14 +19,25 @@ mongoose.connect('mongodb://127.0.0.1:27017/mesto');
 
 const app = express();
 
-app.use('*', cors({
-  origin: [
-    'https://praktikum.tk',
-    'http://praktikum.tk',
-    'localhost:3000'
-  ],
-  credentials: true,
-}))
+
+app.use(function (req, res, next) {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых 
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
+
+// app.use('*', cors({
+//   origin: [
+//     'https://praktikum.tk',
+//     'http://praktikum.tk',
+//     'localhost:3000'
+//   ],
+//   credentials: true,
+// }))
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParse.json());
