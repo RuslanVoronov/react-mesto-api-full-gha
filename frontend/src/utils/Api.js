4c51ad8025192262edc99959cf6736b8f5f353dc
Ruslan Voronov <voronov.rv.99@yandex.ru>
2023-06-08 19:42:24 +0300

    url

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -92,7 +92,7 @@ class Api {
 };
 // https://api.mesto15.nomoredomains.rocks
 const api = new Api({
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'https://api.mesto15.nomoredomains.rocks',
     headers: {
         authorization: localStorage.getItem('token'),
         'Content-Type': 'application/json'
