6b696e140bbde33edb09a645dfa60341d60c6752
Ruslan Voronov <voronov.rv.99@yandex.ru>
2023-06-09 22:04:55 +0300

    modified:   backend/routes/crach-test.js

--- backend/routes/crach-test.js
+++ backend/routes/crach-test.js
@@ -1,9 +1,9 @@
 const crashTest = require('express').Router();
 
 crashTest.get('/', () => {
-    setTimeout(() => {
-        throw new Error('Сервер падает');
-    }, 0);
+  setTimeout(() => {
+    throw new Error('Сервер падает');
+  }, 0);
 });
 
 module.exports = crashTest;
